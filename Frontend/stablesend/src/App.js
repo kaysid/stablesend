e4953579045892c6288deba41c5fef561ab00cbb
kaysid <120425490+kaysid@users.noreply.github.com>
2025-01-23 18:26:19 -0800

    Initial commit

---
+++ Frontend/stablesend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './Bitcoin.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="appHeader">
+        <img src={logo} className="appLogo" alt="logo" />
+        <p className="titleSpan">Stablesend</p>
+        <button>Register as a Business</button>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
