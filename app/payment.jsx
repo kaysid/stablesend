a6067c9212423136c896f9334760b337a3349a37
kaysid <120425490+kaysid@users.noreply.github.com>
2025-01-24 18:48:51 -0800

    Local Pin Authentication and file cleanup

--- app/payment.jsx
+++ app/payment.jsx
@@ -1,38 +1,43 @@
-import { View, Text, StyleSheet, ImageBackground } from 'react-native'
-import React from 'react'
+import { StyleSheet, ImageBackground } from 'react-native';
 import wallpaper from "@/assets/images/wallp.jpg"
 
-const app = () => {
+
+
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const payment = () => {
   return (
     <View style={styles.container}>
+
     <ImageBackground source={wallpaper} style={styles.wallp}>
-      <Text style={styles.titleText}>Stablesend</Text>
+      <Text style={styles.text}>Enter amount</Text>
+      
     </ImageBackground>
+    
     </View>
   )
 }
-
-export default app
+export default payment
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
   },
-  titleText: {
-    color: 'white',
-    fontSize: 42,
+  text: {
+    fontSize: 26,
     fontWeight: 'bold',
     textAlign: 'center',
+    color: 'white',
     textShadowColor: '#000',
-    textShadowOffset: {width: -3, height: 3},
-    textShadowRadius: 4,
+    textShadowOffset: {width: -2, height: 2},
+    textShadowRadius: 3,
   },
   wallp: {
     width: '100%',
     height: '100%',
     flex: 1,
     resizeMode: 'cover',
-    justifyContent: 'center',
   }
-})
+});
