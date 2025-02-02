95afd3532d64c21b0bc28c8b227377de6868c390
kaysid <120425490+kaysid@users.noreply.github.com>
2025-02-02 14:12:00 -0800

    paste button implemented, registered check on launch impl

--- app/screens/settings.jsx
+++ app/screens/settings.jsx
@@ -1,12 +1,29 @@
-import { StyleSheet, TextInput, Text, Button, View } from "react-native";
+import {
+  StyleSheet,
+  TextInput,
+  Text,
+  Button,
+  View,
+  TouchableOpacity,
+} from "react-native";
 import React, { useState, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as Clipboard from "expo-clipboard";
+import Feather from "@expo/vector-icons/Feather";
 
 const Settings = () => {
   const [localEthAddy, onChangeTextEth] = React.useState();
   const [localSolAddy, onChangeTextSol] = React.useState();
-  const [clipboardText, setClipboardText] = useState(""); //need to implement paste button
+
+  const pasteClipboardEth = async () => {
+    const s = await Clipboard.getStringAsync();
+    onChangeTextEth(s);
+  };
+
+  const pasteClipboardSol = async () => {
+    const s = await Clipboard.getStringAsync();
+    onChangeTextSol(s);
+  };
 
   const populateFromLocalStorage = async (params) => {
     //populate from asyncStorage to text input fields
@@ -20,6 +37,16 @@ const Settings = () => {
     //wait for comp to mount before calling
     populateFromLocalStorage();
   }, []);
+
+  useEffect(() => {
+    //making sure stored address updates on variable change
+    storeSol();
+  }, [localSolAddy]);
+
+  useEffect(() => {
+    //making sure stored address updates on variable change
+    storeEth();
+  }, [localEthAddy]);
 
   const storeEth = async (params) => {
     //store eth address to local storage
@@ -52,24 +79,41 @@ const Settings = () => {
   return (
     <View>
       <Text style={styles.textLabel}>Ethereum Network:</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Etherum USDC Receiving Address"
-        placeholderTextColor={"gray"}
-        onChangeText={onChangeTextEth}
-        value={localEthAddy}
-      ></TextInput>
-      {/* <Button onPress={handlePaste} title="Paste"></Button> */}
-      <Button onPress={storeEth} title="Save"></Button>
+      <View style={styles.pasteContainer}>
+        <TextInput
+          editable={false}
+          style={styles.input}
+          placeholder="Etherum USDC Receiving Address"
+          placeholderTextColor={"gray"}
+          onChangeText={onChangeTextEth}
+          value={localEthAddy}
+        ></TextInput>
+        <TouchableOpacity
+          style={styles.touchableO}
+          onPress={pasteClipboardEth()}
+        >
+          <Feather name="clipboard" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
+
       <Text style={styles.textLabel}>Solana Network:</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Solana USDC Receiving Address"
-        placeholderTextColor={"gray"}
-        onChangeText={onChangeTextSol}
-        value={localSolAddy}
-      ></TextInput>
-      <Button onPress={storeSol} title="Save"></Button>
+      <View style={styles.pasteContainer}>
+        <TextInput
+          editable={false}
+          style={styles.input}
+          placeholder="Solana USDC Receiving Address"
+          placeholderTextColor={"gray"}
+          onChangeText={onChangeTextSol}
+          value={localSolAddy}
+        ></TextInput>
+        <TouchableOpacity
+          style={styles.touchableO}
+          onPress={pasteClipboardSol()}
+        >
+          <Feather name="clipboard" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
+
       <Button onPress={clearAddresses} title="Clear Addresses"></Button>
       <Button onPress={print} title="print"></Button>
     </View>
@@ -79,15 +123,24 @@ const Settings = () => {
 export default Settings;
 
 const styles = StyleSheet.create({
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
+  pasteContainer: {
+    flexDirection: "row",
+    height: 50,
     padding: 10,
-    borderRadius: 10,
+    alignItems: "center",
+  },
+  input: {
+    height: "100%",
+    borderWidth: 2,
+    padding: 3,
+    marginRight: 15,
+    flex: 1,
   },
   textLabel: {
     paddingTop: 10,
     paddingLeft: 10,
+  },
+  touchableO: {
+    marginRight: 10,
   },
 });
